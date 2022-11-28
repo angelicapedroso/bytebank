b1aae08b6e5c8665c455322159d958dfd25293ce
angelicapedroso <angelicapedroso05@gmail.com>
2022-11-28 20:01:48 -0300

    feat: cria classe Produto

---
+++ Produto.cs
@@ -0,0 +1,12 @@
+namespace bytebank
+{
+  public class Produto
+  {
+    public string nome;
+    public string fabricante;
+    public string codigoDeBarra;
+    public double valorUnitario;
+    public double valorDeCompra;
+    public int quantidadeEmEstoque;
+  }
+}
