928adc777e9db0c552b46c0fe1171954ec354c69
angelicapedroso <angelicapedroso05@gmail.com>
2022-11-28 19:57:27 -0300

    feat: cria classe ContaCorrent

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using bytebank;
+
+Console.WriteLine("Boas-vindas ao ByteBank");
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "Angélica";
+conta1.conta = "18265-x";
+conta1.numeroAgencia = 55;
+conta1.nomeAgencia = "Agência xyz";
+conta1.saldo = 323;
+
+Console.WriteLine("Titular: " + conta1.titular);
+Console.WriteLine("Conta: " + conta1.conta);
+Console.WriteLine("Número da agência: " + conta1.numeroAgencia);
+Console.WriteLine("Nome da agência: " + conta1.nomeAgencia);
+Console.WriteLine("Saldo: " + conta1.saldo);
+
+Console.ReadKey();
