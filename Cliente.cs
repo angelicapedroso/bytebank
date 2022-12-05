6b14098c34a7ac30148425103cffb678fa3fbc66
angelicapedroso <angelicapedroso05@gmail.com>
2022-12-05 13:10:07 -0300

    feat: implementa campo static para numero de clientes

--- Cliente.cs
+++ Cliente.cs
@@ -2,8 +2,15 @@ namespace bytebank.Titular
 {
   public class Cliente
   {
-    public string nome;
-    public string cpf;
-    public string profissao;
+    public string Nome { get; set; } = "";
+    public string Cpf { get; set; } = "";
+    public string Profissao { get; set; } = "";
+
+    public static int TotalDeClientesCadastrados { get; private set; }
+
+    public Cliente()
+    {
+      TotalDeClientesCadastrados = TotalDeClientesCadastrados + 1;
+    }
   }
 }
